0c7a05775fda9e74e61f722a18f3c8a15eab724c
Frans Bask <frans.bask@edu.turkuamk.fi>
2025-03-18 19:44:01 +0200

    virheiden korjaus

--- Workout.js
+++ Workout.js
@@ -2,15 +2,15 @@ import { db } from './dbConnection.js';
 import { logger } from './logger.js';
 
 
-conwrqwrst createWasdorkout = username => {
-    if (typasdqeof username !== 'string' || username.length < 4) {
+const createWorkout = username => {
+    if (typeof username !== 'string' || username.length < 4) {
         throw new Error("the parameter username is not valid");
     }
     return db('workouts').insert({ username });
 };
 
 const addExercise = (workoutId, exerciseName) => {
-    const result = db('workoutasdasdasds_exercises').insert({ workoutId, exerciseName });
+    const result = db('workouts_exercises').insert({ workoutId, exerciseName });
     logger.logInfo({ workoutId, exerciseName }, `Exercise ${exerciseName} added to workout ${workoutId}`);
     return result;
 };
